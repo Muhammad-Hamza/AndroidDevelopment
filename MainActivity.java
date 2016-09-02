4beb1aa393fdc70d4598f2f90b07044dbe8c2b5c
Muhammad-Hamza <muhammad00hamza@gmail.com>
2016-09-02 05:53:50 -0700

    Geo Quiz Screen Rotation SavedInstance done

--- MainActivity.java
+++ MainActivity.java
@@ -2,11 +2,13 @@ package com.example.panacloud.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
     private Button mTrueButton;
@@ -15,8 +17,45 @@ public class MainActivity extends AppCompatActivity {
     private TextView mQuestionTextView ;
     //private TextView mTextclicker;
     private ImageButton mPrevButton;
+  private static final String TAG = "MainActivity";
+    private static final String KEY_INDEX = "index";
 
 
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        Log.d(TAG,"onStart Called");
+
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        Log.d(TAG,"onPause Called");
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        Log.d(TAG,"onStop Called");
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        Log.d(TAG,"onResume Called");
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy Called");
+    }
 
     private Questions[] mQuestionbank = new Questions[]
             {
@@ -59,7 +98,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate (Bundle) called");
+
         setContentView(R.layout.activity_main);
+
+
 
        mQuestionTextView =(TextView)findViewById(R.id.question_text_view);
         //int question = mQuestionbank[mCurrentIndex].getTextResid();
@@ -98,6 +141,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                if(mCurrentIndex==0)
+                {
+                    mCurrentIndex = mQuestionbank.length;
+                }
 
                     mCurrentIndex = (mCurrentIndex - 1) % mQuestionbank.length;
 
@@ -114,8 +161,23 @@ public class MainActivity extends AppCompatActivity {
                 updateQuestions();
             }
         });*/
+        if (savedInstanceState != null) {             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);         }
 
         updateQuestions();
 
+
+
+
+
+
     }
+    @Override
+    public void onSaveInstanceState(Bundle SavedInstanceState){
+        super.onSaveInstanceState(SavedInstanceState);
+        Log.i(TAG,"SavedInstanceState");
+        SavedInstanceState.putInt(KEY_INDEX,mCurrentIndex);
+
+
+   }
+
 }
